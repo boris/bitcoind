bd14f5af1eb3d788915a49f27992869512c995e1
Boris Quiroz <borisquiroz@pm.me>
2025-01-14 07:13:41 -0300

    Switch to requests lib

--- rpc/auth.py
+++ rpc/auth.py
@@ -1,18 +1,39 @@
-from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
+import json
+import requests
+from requests.auth import HTTPBasicAuth
 
 class BitcoinRPC():
     def __init__(self, rpc_user, rpc_password):
         self.rpc_user = rpc_user
         self.rpc_password = rpc_password
-        self.rpc_connection = AuthServiceProxy(f"http://{self.rpc_user}:{self.rpc_password}@192.168.1.30:8332",
-                                               timeout=10)
+        self.rpc_connection = "https://btc.borisquiroz.dev"
+    
+    def _call_rpc(self, method, params=None):
+        headers = {'content-type': 'application/json'}
+        payload = json.dumps({
+            "jsonrpc": "1.0",
+            "id": "python-bitcoinrpc",
+            "method": method,
+            "params": params or []
+        })
+        response = requests.post(
+            self.rpc_connection,
+            data=payload,
+            headers=headers,
+            auth=HTTPBasicAuth(self.rpc_user, self.rpc_password),
+        )
 
+        response.raise_for_status()
+        return response.json()
 
     def getblockcount(self):
-        msg = f"Current block count: {self.rpc_connection.getblockcount()}"
+        response = self._call_rpc("getblockcount")
+        msg = f"Current block count: {response['result']}"
         return msg
 
     def getchaintips(self):
-        return self.rpc_connection.getchaintips()
+        response = self._call_rpc("getchaintips")
+        msg = f"Chain tips: {response['result']}"
+        return msg
 
 
